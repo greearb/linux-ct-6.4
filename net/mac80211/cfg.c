b0ffcddd3d7966c44eed6cbe79c41a6baddad68f
Ben Greear <greearb@candelatech.com>
2017-11-03 10:51:17 -0700

    wifi: Add error messages for station-add failure path.
    Should help debug roaming and other bugs.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- net/mac80211/cfg.c
+++ net/mac80211/cfg.c
@@ -474,11 +474,15 @@ static int ieee80211_add_key(struct wiphy *wiphy, struct net_device *dev,
 	struct ieee80211_key *key;
 	int err;
 
-	if (!ieee80211_sdata_running(sdata))
+	if (!ieee80211_sdata_running(sdata)) {
+		sdata_info(sdata, "add-key failed, sdata is not running.\n");
 		return -ENETDOWN;
+	}
 
-	if (IS_ERR(link))
+	if (IS_ERR(link)) {
+		sdata_info(sdata, "add-key failed, link is error: %p", link);
 		return PTR_ERR(link);
+	}
 
 	if (pairwise && params->mode == NL80211_KEY_SET_TX)
 		return ieee80211_set_tx(sdata, mac_addr, key_idx);
@@ -488,10 +492,14 @@ static int ieee80211_add_key(struct wiphy *wiphy, struct net_device *dev,
 	case WLAN_CIPHER_SUITE_WEP40:
 	case WLAN_CIPHER_SUITE_TKIP:
 	case WLAN_CIPHER_SUITE_WEP104:
-		if (link_id >= 0)
+		if (link_id >= 0) {
+			sdata_info(sdata, "add-key failed, link-id >= 0: %d.\n", link_id);
 			return -EINVAL;
-		if (WARN_ON_ONCE(fips_enabled))
+		}
+		if (WARN_ON_ONCE(fips_enabled)) {
+			sdata_info(sdata, "add-key failed, wep/tkip, wep failed.\n");
 			return -EINVAL;
+		}
 		break;
 	default:
 		break;
@@ -499,8 +507,10 @@ static int ieee80211_add_key(struct wiphy *wiphy, struct net_device *dev,
 
 	key = ieee80211_key_alloc(params->cipher, key_idx, params->key_len,
 				  params->key, params->seq_len, params->seq);
-	if (IS_ERR(key))
+	if (IS_ERR(key)) {
+		sdata_info(sdata, "add-key failed, key-alloc failed: %ld\n", PTR_ERR(key));
 		return PTR_ERR(key);
+	}
 
 	key->conf.link_id = link_id;
 
@@ -526,6 +536,9 @@ static int ieee80211_add_key(struct wiphy *wiphy, struct net_device *dev,
 		 */
 		if (!sta || !test_sta_flag(sta, WLAN_STA_ASSOC)) {
 			ieee80211_key_free_unused(key);
+			sdata_info(sdata, "add-key failed, FT key-set called before association completed?  Supplicant can retry, sta: %p  assoc: %d...\n",
+				   sta, sta ? test_sta_flag(sta, WLAN_STA_ASSOC) : -1);
+
 			err = -ENOENT;
 			goto out_unlock;
 		}
