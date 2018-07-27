9afdc1ac2af2ff38474324c5f871eee2f43ead70
Ben Greear <greearb@candelatech.com>
2018-07-26 17:01:11 -0700

    ethtool: Guard ethtool stats2 with define
    This should help backports work more easily.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- include/linux/ethtool.h
+++ include/linux/ethtool.h
@@ -840,6 +840,8 @@ struct ethtool_ops {
 	int	(*set_phys_id)(struct net_device *, enum ethtool_phys_id_state);
 	void	(*get_ethtool_stats)(struct net_device *,
 				     struct ethtool_stats *, u64 *);
+/* Make backporting easier */
+#define HAS_ETHTOOL_STATS2 1
 	void	(*get_ethtool_stats2)(struct net_device *,
 				      struct ethtool_stats *, u64 *, u32 level);
 	int	(*begin)(struct net_device *);
