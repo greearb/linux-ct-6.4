d50b59691d4f12711b5125368b0baac81c60596d
Ben Greear <greearb@candelatech.com>
2021-07-15 15:38:13 -0700

    wifi: mt76: mt7915: allow processing TXS for 'NO_SKB' pkt-ids
    This will let us update stats and wcid.rate for every TXS
    callback we receive for a particular wcid.
    
    For now, the TXS is not requested for NO_SKB frames, however.
    That will be allowed in next patch.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7915/mac.c
+++ drivers/net/wireless/mediatek/mt76/mt7915/mac.c
@@ -1070,7 +1070,7 @@ static void mt7915_mac_add_txs(struct mt7915_dev *dev, void *data)
 	wcidx = le32_get_bits(txs_data[2], MT_TXS2_WCID);
 	pid = le32_get_bits(txs_data[3], MT_TXS3_PID);
 
-	if (pid < MT_PACKET_ID_WED)
+	if (pid < MT_PACKET_ID_NO_SKB)
 		return;
 
 	if (wcidx >= mt7915_wtbl_size(dev))
