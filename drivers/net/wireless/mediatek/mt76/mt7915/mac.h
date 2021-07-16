9d416a710082bea03f34e6e8df18103b57d5c28b
Ben Greear <greearb@candelatech.com>
2021-07-16 07:23:11 -0700

    wifi: mt76: mt7915: add note about TXSFM 0x2
    This format needs a special command to enable, and is not enabled
    by default.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7915/mac.h
+++ drivers/net/wireless/mediatek/mt76/mt7915/mac.h
@@ -12,6 +12,9 @@
 #define MT_TX_FREE_MPDU_HEADER		BIT(30)
 #define MT_TX_FREE_MSDU_ID_V3		GENMASK(14, 0)
 
+/* The F0 variables are for TXSFM 0x0 and 0x1.  The F1 variables
+ * are for TXSFM 0x2 aka PPDU format.
+ */
 #define MT_TXS5_F0_FINAL_MPDU		BIT(31)
 #define MT_TXS5_F0_QOS			BIT(30)
 #define MT_TXS5_F0_TX_COUNT		GENMASK(29, 25)
