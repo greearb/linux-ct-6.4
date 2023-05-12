debdac5544ece46446421099bb4327c1fcfcc701
Ben Greear <greearb@candelatech.com>
2023-05-12 14:02:59 -0700

    wifi: mt76: mt7921: Enable spectrum management flag.
    This hardware does appear to support changing power dynamically.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7921/init.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/init.c
@@ -139,6 +139,7 @@ mt7921_init_wiphy(struct ieee80211_hw *hw)
 	ieee80211_hw_set(hw, SUPPORTS_TX_ENCAP_OFFLOAD);
 	ieee80211_hw_set(hw, SUPPORTS_RX_DECAP_OFFLOAD);
 	ieee80211_hw_set(hw, WANT_MONITOR_VIF);
+	ieee80211_hw_set(hw, SPECTRUM_MGMT);
 	ieee80211_hw_set(hw, SUPPORTS_PS);
 	ieee80211_hw_set(hw, SUPPORTS_DYNAMIC_PS);
 	ieee80211_hw_set(hw, SUPPORTS_VHT_EXT_NSS_BW);
