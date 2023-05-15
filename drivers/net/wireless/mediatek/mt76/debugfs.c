greearb/linux-ct-5.17

mt76: mt7921: force txs for each skb.
This is full mapping of tx status to skb, not just txstatus
for 'no-skb-id' logic.  Seems it breaks the txs processing
entirely at high speeds...

Signed-off-by: Ben Greear <greearb@candelatech.com>

drivers/net/wireless/mediatek/mt76/mt7921/debugfs.c
@@ -666,6 +666,29 @@ mt7921_txs_for_no_skb_get(void *data, u64 *val)
 DEFINE_DEBUGFS_ATTRIBUTE(fops_txs_for_no_skb, mt7921_txs_for_no_skb_get,
 			 mt7921_txs_for_no_skb_set, "%lld\n");
 
+static int
+mt7921_txs_for_all_set(void *data, u64 val)
+{
+	struct mt7921_dev *dev = data;
+
+	dev->txs_for_all_enabled = !!val;
+
+	return 0;
+}
+
+static int
+mt7921_txs_for_all_get(void *data, u64 *val)
+{
+	struct mt7921_dev *dev = data;
+
+	*val = dev->txs_for_all_enabled;
+
+	return 0;
+}
+
+DEFINE_DEBUGFS_ATTRIBUTE(fops_txs_for_all, mt7921_txs_for_all_get,
+			 mt7921_txs_for_all_set, "%lld\n");
+
 int mt7921_init_debugfs(struct mt7921_dev *dev)
 {
 	struct dentry *dir;
@@ -695,6 +718,7 @@ int mt7921_init_debugfs(struct mt7921_dev *dev)
 	debugfs_create_file("set_rate_override", 0600, dir,
 			    dev, &fops_set_rate_override);
 	debugfs_create_file("force_txs", 0600, dir, dev, &fops_txs_for_no_skb);
+	debugfs_create_file("force_txs_all_skb", 0600, dir, dev, &fops_txs_for_all);
 	/* TODO:  Add rate_txpower_show */
 	return 0;
 }

greearb/linux-ct-6.4

drivers/net/wireless/mediatek/mt76/debugfs.c
@@ -112,6 +112,28 @@ mt7915_txs_for_no_skb_get(void *data, u64 *val)
 DEFINE_DEBUGFS_ATTRIBUTE(fops_txs_for_no_skb, mt7915_txs_for_no_skb_get,
 			 mt7915_txs_for_no_skb_set, "%lld\n");
 
+static int
+mt76_txs_for_all_set(void *data, u64 val)
+{
+	struct mt76_dev *dev = data;
+
+	dev->txs_for_all_enabled = !!val;
+
+	return 0;
+}
+
+static int
+mt76_txs_for_all_get(void *data, u64 *val)
+{
+	struct mt76_dev *dev = data;
+
+	*val = dev->txs_for_all_enabled;
+
+	return 0;
+}
+
+DEFINE_DEBUGFS_ATTRIBUTE(fops_txs_for_all, mt76_txs_for_all_get,
+			 mt76_txs_for_all_set, "%lld\n");
 
 void mt76_seq_puts_array(struct seq_file *file, const char *str,
 			 s8 *val, int len)
@@ -143,6 +165,7 @@ mt76_register_debugfs_fops(struct mt76_phy *phy,
 	debugfs_create_file_unsafe("napi_threaded", 0600, dir, dev,
 				   &fops_napi_threaded);
 	debugfs_create_file("force_txs", 0600, dir, dev, &fops_txs_for_no_skb);
+	debugfs_create_file("force_txs_all_skb", 0600, dir, dev, &fops_txs_for_all);
 	debugfs_create_blob("eeprom", 0400, dir, &dev->eeprom);
 	if (dev->otp.data)
 		debugfs_create_blob("otp", 0400, dir, &dev->otp);